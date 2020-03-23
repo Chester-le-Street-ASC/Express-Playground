6b801d3c80980d1e79d52381c5162d94555bdb7c
Chris Heppell <clheppell@icloud.com>
2020-03-23 01:03:23 +0000

    For test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express', headers: JSON.stringify(req.headers) });
+  res.json(req.headers);
 });
 
 module.exports = router;
